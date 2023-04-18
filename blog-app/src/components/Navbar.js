77b7e7ed709964c02cc3bb73eab2f53f5cf601bf
Ethan Straub <evstraub@gmail.com>
2023-04-18 14:32:42 -0400

    added basic layout of components

---
+++ blog-app/src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <h1>Ethan's Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New blog</a>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
