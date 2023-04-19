5cfb266443dee9a9076ffa5f2eb216273d4929e5
Ethan Straub <evstraub@gmail.com>
2023-04-19 16:15:00 -0400

    added fetch hook to BlogDetails and added styling

--- blog-app/src/BlogDetails.js
+++ blog-app/src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom"
+import useFetch from "./components/useFetch"
 
 const BlogDetails = () => {
 const { id } = useParams()
-
+const {data:blog, error, isLoading} =useFetch("http://localhost:8000/blogs/" + id)
   return (
-    <div>BlogDetails {id}</div>
+   <div className="blog-details">
+    {isLoading && <div>Loading...</div>}
+    {error && <div>{error}</div>}
+    {blog && (
+        <article>
+            <h2>{blog.title}</h2>
+            <p> Written by {blog.author}</p>
+            <div>{blog.body}</div>
+        </article>
+    )}
+   </div>
   )
 }
 
