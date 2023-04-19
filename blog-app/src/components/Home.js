32e25ce3ef4741fd416d4857a42d3d1773d14148
Ethan Straub <evstraub@gmail.com>
2023-04-19 10:07:50 -0400

    added a  reusable custom fetch hook "useFetch" that fetches data from the server

--- blog-app/src/components/Home.js
+++ blog-app/src/components/Home.js
@@ -1,33 +1,13 @@
 
 import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-const [blogs, setBlogs] = useState(null);
-const [isLoading, setIsLoading] = useState(true)
-const [error, setError] = useState(null)
+const { data:blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
 
 
-useEffect(()=> {
- fetch("http://localhost:8000/blogs")
-.then(res => {
-    if(!res.ok){
-        throw Error("couldn't get resource")
-    }
-
-     return res.json() 
-})
-  .then(data =>{
- setBlogs(data)
- setIsLoading(false)
- setError(null)
-})
-.catch(err => {
-setError(err.message)
-setIsLoading(false)
-})
-},[])
 
   return (
     <div className="home">
