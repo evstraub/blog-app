9f653879f3c7aacfadfd75e2318582c6a9ca88ad
Ethan Straub <evstraub@gmail.com>
2023-04-19 17:08:11 -0400

    added not found page route

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -3,6 +3,7 @@ import BlogDetails from './BlogDetails';
 import Create from './components/Create';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route exact path="/blogs/:id">
               <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound/>
             </Route>
           </Switch>
         </div>
